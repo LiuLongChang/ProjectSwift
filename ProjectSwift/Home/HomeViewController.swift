157c4cd51b0e8e4d6a4cbd53617a70435022711b
LiuLongChang <1040719208@qq.com>
2016-01-12 10:01:59 +0800

    Home页面
    Banner图 和 四个选择条目

--- ProjectSwift/Home/HomeViewController.swift
+++ ProjectSwift/Home/HomeViewController.swift
@@ -11,6 +11,11 @@ import SnapKit
 
 
 
+
+let rowHeight : CGFloat = 117
+
+
+
 class HomeCell: UITableViewCell {
     
     var imagView = UIImageView()
@@ -19,25 +24,17 @@ class HomeCell: UITableViewCell {
         
         super.init(style: style, reuseIdentifier: reuseIdentifier)
             self.contentView.addSubview(imagView)
-            imageView!.snp_makeConstraints { (make) -> Void in
-                make.left.equalTo(0)
-                make.right.equalTo(0)
-                make.top.equalTo(0)
-                make.bottom.equalTo(0)
-            }
-        
+            imagView.frame = CGRectMake(0, 0, ScreenWidth, rowHeight)
     }
 
      required init?(coder aDecoder: NSCoder) {
          fatalError("init(coder:) has not been implemented")
      }
     
-    
-    
 }
 
 
-class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewDataSource {
+class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate {
     
     
     @IBOutlet weak var tabView: UITableView!
@@ -45,7 +42,7 @@ class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewData
     
     
     var imgAry = ["checkCar22","iAgency22","acci22","NewActive22"]
-    
+    let pageControl = UIPageControl(frame: CGRectMake(0,0,100,30))
     
 
     override func viewDidLoad() {
@@ -55,33 +52,66 @@ class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewData
         
         tabView.delegate = self
         tabView.dataSource = self
+        tabView.separatorStyle = .None
+        tabView.showsHorizontalScrollIndicator = false
+        tabView.showsVerticalScrollIndicator = false
         
         tabView.snp_makeConstraints { (make) -> Void in
-            make.top.equalTo(self.view).offset(69)
-            make.left.equalTo(self.view).offset(5)
-            make.bottom.equalTo(self.view).offset(-5)
-            make.right.equalTo(self.view).offset(-5)
+            make.top.equalTo(self.view).offset(0)
+            make.left.equalTo(self.view).offset(0)
+            make.bottom.equalTo(self.view).offset(0)
+            make.right.equalTo(self.view).offset(0)
         }
         
         
         
-        let scrollView = UIScrollView()
+        let scrollView = UIScrollView(frame: CGRectMake(0,0,ScreenWidth,170))
+        scrollView.delegate = self
         self.tabView.tableHeaderView = scrollView
-        scrollView.snp_makeConstraints { (make) -> Void in
-            make.height.equalTo(250)
-            make.width.equalTo(ScreenWidth)
-//            make.top.equalTo(self.view).offset(0)
-//            make.left.equalTo(self.view).offset(0)
-//            make.right.equalTo(self.view).offset(0)
-        }
         scrollView.backgroundColor = UIColor.grayColor()
         
+        let bannerAry = ["checkYear":0,"promise":1,"shareHasPresent":2,"bannerCheck":3]
+        for (imgStr,i) in bannerAry {
+            let imaView = UIImageView(frame: CGRectMake( CGFloat(i) * ScreenWidth, 0, ScreenWidth, 170))
+            imaView.image = UIImage(named: imgStr)
+            imaView.userInteractionEnabled = true
+            scrollView.addSubview(imaView)
+            imaView.tag = 100 + i
+            imaView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "tapBanberAction:"))
+        }
         
+        scrollView.contentSize =  CGSizeMake(ScreenWidth * CGFloat(bannerAry.count) , 0)
+        scrollView.pagingEnabled = true
+        
+        
+        
+        
+        pageControl.numberOfPages = bannerAry.count
+        pageControl.center = CGPointMake(self.view.center.x, 140)
+        self.tabView.addSubview(pageControl)
         
         
         // Do any additional setup after loading the view.
     }
 
+    func tapBanberAction(tap: UITapGestureRecognizer){
+    
+        let view = tap.view as! UIImageView
+    
+        print("\(view.tag)")
+    
+    
+    }
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+    
+        pageControl.currentPage =  Int(scrollView.contentOffset.x /  ScreenWidth)
+        
+    }
+    
+    
+    
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
@@ -98,7 +128,7 @@ class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewData
         if cell == nil {
             
             cell = HomeCell(style: .Default, reuseIdentifier: ID)
-
+            cell?.selectionStyle = .None
             
         }
         
@@ -111,7 +141,7 @@ class HomeViewController: BaseViewController,UITableViewDelegate,UITableViewData
     
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 200
+        return rowHeight+10
     }
     
     override func didReceiveMemoryWarning() {
